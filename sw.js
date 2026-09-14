6c450bd9a08679623b30fb057e22fb5c9bb433c7
pmirisola16-prog <pmirisola16@gmail.com>
2026-09-14 09:22:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Alza il numero di versione a ogni deploy: forza l'aggiornamento sui telefoni
 // dove l'app è già installata.
-const CACHE = 'grandline-v5';
+const CACHE = 'grandline-v6';
 const FILES = ['./', './index.html', './manifest.json', './personaggi.csv',
                './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
